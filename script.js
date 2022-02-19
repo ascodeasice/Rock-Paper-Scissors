3a323a01caf8c18434ec58841a6a894656f39aa9
ascodeasice <ab0988956087@gmail.com>
2022-02-19 10:54:08 +0800

    finsih the basic part without html and css elements

---
+++ script.js
@@ -0,0 +1,50 @@
+function computerPlay(){
+    move=["Rock","Paper","Scissors"];
+    randomInt=Math.floor(3*Math.random())
+    return move[randomInt];
+}
+
+function standardizeMove(playerMove){
+    result="";
+    result+=playerMove[0].toUpperCase();
+    result+=playerMove.substr(1).toLowerCase();
+    return result;
+}
+
+function playerWon(playerMove,computerMove){
+    return (playerMove=="Paper"&&computerMove=="Rock")||(playerMove=="Rock"&&computerMove=="Scissors")
+    ||(playerMove=="Scissors"&&computerMove=="Paper");
+}
+
+function playRound(playerMove,computerMove){
+    playerMove=standardizeMove(playerMove);
+    if(playerMove==computerMove)
+        return `It's a tie,both of you played ${playerMove}`;
+    else if(playerWon(playerMove,computerMove))
+        return `You Win! ${playerMove} beats ${computerMove}`;
+    else 
+        return `You Lose! ${computerMove} beats ${playerMove}`;
+}
+
+function invalid(playerMove){
+    if(playerMove=="")
+        return true;
+    playerMove=standardizeMove(playerMove);
+    return playerMove!="Rock"&&playerMove!="Paper"&&playerMove!="Scissors";
+}
+
+var game_time=5;
+
+function game(){
+    for(let i=0;i<game_time;i++){
+        let playerMove="";
+        while(invalid(playerMove)){
+            playerMove=prompt("Enter your move(Rock,Paper,Scissors):")
+            if(invalid(playerMove))
+                console.log("Invalid Move,try again");
+        }
+        console.log(playRound(playerMove,computerPlay()));
+    }
+}
+
+game();
