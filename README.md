27e349950ecfa0c25fbecc1fd5ddeeba5a507b05
ascodeasice <ab0988956087@gmail.com>
2023-02-09 15:44:14 +0800

    Add bootstrap for css

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Rock-Paper-Scissors
-Practice of The Odin Project
+Practice of The Odin Project, a basic rock paper scissors website
+# Details
+- Use bootstrap for css
+# Live Preview
+[Link]()
--- script.js
+++ script.js
@@ -1,56 +1,68 @@
-var playerScore=0;
-var computerScore=0;
-const btns=document.querySelectorAll("button");
-const resultDiv=document.querySelector("#result")
-const playerScoreP=document.querySelector("#playerScore")
-const computerScoreP=document.querySelector("#computerScore")
-var over=false;
+var playerScore = 0;
+var computerScore = 0;
+const btns = document.querySelectorAll("button");
+const resultDiv = document.querySelector("#result")
+const playerScoreText = document.querySelector("#playerScore")
+const computerScoreText = document.querySelector("#computerScore")
+const resetBtn = document.querySelector("#resetBtn");
+var over = false;
 
-function computerPlay(){
-    move=["Rock","Paper","Scissors"];
-    randomInt=Math.floor(3*Math.random())
+function computerPlay() {
+    move = ["Rock", "Paper", "Scissors"];
+    randomInt = Math.floor(3 * Math.random())
     return move[randomInt];
 }
 
-function standardizeMove(playerMove){
-    let result="";
-    result+=playerMove[0].toUpperCase();
-    result+=playerMove.substr(1).toLowerCase();
+function standardizeMove(playerMove) {
+    let result = "";
+    result += playerMove[0].toUpperCase();
+    result += playerMove.substr(1).toLowerCase();
     return result;
 }
 
-function playerWon(playerMove,computerMove){
-    return (playerMove=="Paper"&&computerMove=="Rock")||(playerMove=="Rock"&&computerMove=="Scissors")
-    ||(playerMove=="Scissors"&&computerMove=="Paper");
+function playerWon(playerMove, computerMove) {
+    return (playerMove == "Paper" && computerMove == "Rock") || (playerMove == "Rock" && computerMove == "Scissors")
+        || (playerMove == "Scissors" && computerMove == "Paper");
 }
 
-function playRound(playerMove,computerMove){
-    playerMove=standardizeMove(playerMove);
-    if(playerMove==computerMove)
+function playRound(playerMove, computerMove) {
+    playerMove = standardizeMove(playerMove);
+    if (playerMove == computerMove)
         return `It's a tie,both of you played ${playerMove}`;
-    else if(playerWon(playerMove,computerMove)){
+    else if (playerWon(playerMove, computerMove)) {
         playerScore++;
-        playerScoreP.innerText=`Player score:${playerScore}`;
-        if(playerScore>=5){
-            over=true;
+        playerScoreText.innerText = `Player score:${playerScore}`;
+        if (playerScore >= 5) {
+            over = true;
+            resetBtn.classList.remove("invisible");
             return "You win!You reached 5 points first!"
         }
         return `You Win! ${playerMove} beats ${computerMove}`;
     }
     else {
         computerScore++;
-        computerScoreP.innerText=`Computer score:${computerScore}`;
-        if(computerScore>=5){
-            over=true;
+        computerScoreText.innerText = `Computer score:${computerScore}`;
+        if (computerScore >= 5) {
+            over = true;
+            resetBtn.classList.remove("invisible");
             return "You lose!The computer reached 5 points first!"
         }
         return `You Lose! ${computerMove} beats ${playerMove}`;
     }
 }
 
-btns.forEach(btn=>btn.addEventListener("click",function(e){
-    if(over)
+btns.forEach(btn => btn.addEventListener("click", function (e) {
+    if (over)
         return;
-    resultDiv.innerText=playRound(e.target.id,computerPlay())
+    resultDiv.innerText = playRound(e.target.id, computerPlay())
 }))
 
+resetBtn.addEventListener("click", function (e) {
+    e.target.classList.add("invisible");
+    over = false;
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreText.innerText = "Player score:0";
+    computerScoreText.innerText = "Computer score:0";
+    resultDiv.innerText = "";
+})
